97bcb0f5b1278bfee01e940e4ebdf88a35573133
Nick-Massagli <mas19015@byui.edu>
2023-11-21 15:59:27 -0700

    msg

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,3 +1,20 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
 document.getElementById("lastModified").innerHTML = new Date(document.lastModified);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const day = new Date().getDay();
+    const banner = document.querySelector('#reminder-banner');
+
+    if ((day >= 1) && (day <= 3)) {
+        banner.style.display = 'block';
+    } else {
+        banner.style.display = 'none';
+    }
+});
+
+function closeBanner() {
+    var banner = document.querySelector('#reminder-banner');
+    banner.style.display = 'none';
+};
